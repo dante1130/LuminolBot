16666c2e1ea2acd54dced9d69f358f37d5410377
Donte <andrewdante2@gmail.com>
2021-01-31 17:24:12 +0800

    Fixed capital cases

--- commands/tth.js
+++ commands/tth.js
@@ -1,31 +1,24 @@
-const { MessageEmbed } = require('discord.js');
-
 module.exports = {
     name: 'tth',
     description: "Replaces t with th and th with t.",
 
     execute(message, args) {
-        const embed = new MessageEmbed();
         var newMessage = "";
         args = args.join(' ');
 
         for (let i = 0; i < args.length; i++) {
-            if (args[i] === "t") {
+            if (args[i].toLowerCase() === "t") {
                 if (args[i+1] === "h") {
-                    newMessage += "t";
+                    newMessage += args[i];
                     i++;
                 } else {
-                    newMessage += "th";
+                    newMessage += args[i] + "h";
                 }
             } else {
                 newMessage += args[i];
             }
-            console.log(newMessage);
         }
         
-        embed.setTitle(args)
-            .setDescription(newMessage)
-            .setColor('#00FFFF');
-        message.channel.send(embed);
+        message.channel.send(newMessage);
     }
 }
